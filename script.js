667a18624618d840b36d1dbb092f7e2016ae396e
sanjana <sanjana.bansal@loblaw.ca>
2024-02-26 23:13:14 -0500

    added gif and invitation text

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-document.getElementById('book').addEventListener('click', function() {
-    /*
-    if (this.classList.contains('close')) {
-      this.classList.remove('close');
-      this.classList.add('open');
-    } else if (this.classList.contains('open')) {
-      this.classList.remove('open');
-      this.classList.add('open2');
-    } else if (this.classList.contains('open2')) {
-      this.classList.remove('open2');
-      this.classList.add('close');
-    }
-    */
-
-    this.classList.toggle('open');
-  });
-  
+document.getElementById('book').addEventListener('click', function () {
+  this.classList.toggle('open');
+});
